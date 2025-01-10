6064491af6925f0bb1c89d3e8a4928856178a542
Dang Tu <dtu161205@gmail.com>
2025-01-10 19:01:40 +0700

    Tạo giao diện sản phẩm nổi bật

--- middlewares/client/products/Home.Middleware.js
+++ middlewares/client/products/Home.Middleware.js
@@ -1,10 +1,27 @@
 const Tree = require("../../../helpers/products/buildCategoryTree.helper");
 const categories = require("../../../models/categories/category.model");
+const Products = require("../../../models/products/products");
+const accounting = require("accounting");
 module.exports.locals = async (req, res, next) => {
+  /// Lấy ra các danh mục
   const categoties = await categories.find({
     deleted: false,
     status: "active",
   });
+  /// Lấy ra sản phẩm nổi bật
+  const featuredProducts = await Products.find({
+    deleted: false,
+    status: "active",
+    featured: "1",
+  });
+  const Price = await Promise.all(
+    featuredProducts.map(async (item) => {
+      const price = accounting.formatMoney(item.Price, "₫", 0, ".", ",");
+      return price;
+    })
+  );
   res.locals.newCategory = Tree(categoties);
+  res.locals.featuredProducts = featuredProducts;
+  res.locals.priceArray = Price;
   next();
 };
